9359f6f6dd3e9ef049c5e926b5026e9a5ae45e65
Rajat-Chowdhury <crajat2000@gmail.com>
2020-08-11 20:41:15 +0530

    about to eject project

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import classes from './Header.css';
+
+import ForumIcon from '@material-ui/icons/Forum';
+import PersonIcon from '@material-ui/icons/Person';
 
 const Header = () => {
 
 
     return(
-        <div>
+        <div className={classes.header} >
+            <PersonIcon/>
             <h2>I am a heaeder</h2>
+            <ForumIcon/>
         </div>
     )
 
